2b3885a64fe2dbaf3f019c8a41f863314a54a68a
Khaled Altabtbaei <36083988+khalidtab@users.noreply.github.com>
2023-10-27 13:13:47 -0600

    Trying a new way to install packages

--- workflow/envs/dada2.post-deploy.sh
+++ workflow/envs/dada2.post-deploy.sh
@@ -1,2 +1,1 @@
-conda activate $CONDA_PREFIX
-Rscript -e 'install.packages("BiocManager",repos = "http://cran.us.r-project.org"); BiocManager::install("dada2"); install.packages(c("readr","magrittr","optparse","tidyr","jsonlite","furrr","future"),repos="http://cran.us.r-project.org")'
+/bin/bash -c 'conda run -n $CONDA_PREFIX Rscript install_packages.R'
