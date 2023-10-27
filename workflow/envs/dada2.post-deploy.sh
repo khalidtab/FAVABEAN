57b7cf937ef31e60d89b19f16f6f706cd78a2f76
Khaled Altabtbaei <36083988+khalidtab@users.noreply.github.com>
2023-10-27 14:44:07 -0600

    trying new deployment method

--- workflow/envs/dada2.post-deploy.sh
+++ workflow/envs/dada2.post-deploy.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+# Initialize conda
+. /opt/conda/etc/profile.d/conda.sh
+
+# Activate the specific conda environment
 conda activate $CONDA_PREFIX
+
+# Navigate to the desired directory
 cd ./workflow/envs/
+
+# Run the R script to install packages
 Rscript install_packages.R
