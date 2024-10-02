64b792b175782f0d8e7522387f4b777965de9782
Khaled Altabtbaei <36083988+khalidtab@users.noreply.github.com>
2024-10-02 00:40:09 -0600

    Update install_packages.R

--- workflow/envs/install_packages.R
+++ workflow/envs/install_packages.R
@@ -1,10 +1,9 @@
 #!usr/bin/env Rscript
 
-
-#install.packages("devtools")
-#library("devtools")
-#devtools::install_github("benjjneb/dada2")
-
 BiocManager::install("optparse")
 install.packages(c("readr", "magrittr", "optparse", "tidyr", "jsonlite","data.table"), repos="http://cran.us.r-project.org",verbose=TRUE,quiet=FALSE)
-BiocManager::install("dada2")
+#BiocManager::install("dada2")
+
+install.packages("devtools")
+library("devtools")
+devtools::install_github("benjjneb/dada2")
