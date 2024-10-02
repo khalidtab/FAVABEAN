734039de0796a2ad9dc1d9ac3bf6079a730c77ed
Khaled Altabtbaei <36083988+khalidtab@users.noreply.github.com>
2024-10-02 00:45:34 -0600

    Trying installing DADA2 via devtools

--- workflow/envs/install_packages.R
+++ workflow/envs/install_packages.R
@@ -4,6 +4,6 @@ BiocManager::install("optparse")
 install.packages(c("readr", "magrittr", "optparse", "tidyr", "jsonlite","data.table"), repos="http://cran.us.r-project.org",verbose=TRUE,quiet=FALSE)
 #BiocManager::install("dada2")
 
-install.packages("devtools")
+#install.packages("devtools")
 library("devtools")
 devtools::install_github("benjjneb/dada2")
