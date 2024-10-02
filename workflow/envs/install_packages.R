db92b4203465b97a46f94125fca9b1f7f210f4a8
Khaled Altabtbaei <36083988+khalidtab@users.noreply.github.com>
2024-10-02 01:28:53 -0600

    bioconductor dada2 installation trial

--- workflow/envs/install_packages.R
+++ workflow/envs/install_packages.R
@@ -1,9 +1,10 @@
 #!usr/bin/env Rscript
 
-BiocManager::install("optparse")
-install.packages(c("readr", "magrittr", "optparse", "tidyr", "jsonlite","data.table"), repos="http://cran.us.r-project.org",verbose=TRUE,quiet=FALSE)
-#BiocManager::install("dada2")
 
 #install.packages("devtools")
-library("devtools")
-devtools::install_github("benjjneb/dada2")
+#library("devtools")
+#devtools::install_github("benjjneb/dada2")
+
+BiocManager::install("optparse",ask=FALSE,force=TRUE,update=FALSE)
+BiocManager::install(c("readr", "magrittr", "tidyr", "jsonlite","data.table"),ask=FALSE,force=TRUE,update=FALSE)
+BiocManager::install("dada2",,ask=FALSE,force=TRUE,update=FALSE)
