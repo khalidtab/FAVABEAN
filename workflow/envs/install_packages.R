2186f745a7e1208eaa5c8ce0b15abb2c1cfab1b6
Khaled Altabtbaei <36083988+khalidtab@users.noreply.github.com>
2024-10-02 08:52:18 -0600

    DADA2 installation clean up

--- workflow/envs/install_packages.R
+++ workflow/envs/install_packages.R
@@ -1,10 +1,16 @@
 #!usr/bin/env Rscript
 
 
-#install.packages("devtools")
-#library("devtools")
-#devtools::install_github("benjjneb/dada2")
+# Install core packages without updating other packages
+BiocManager::install("optparse", ask=FALSE, force=TRUE, update=FALSE)
+BiocManager::install(c("readr", "magrittr", "tidyr", "jsonlite", "data.table"), ask=FALSE, force=TRUE, update=FALSE)
 
-BiocManager::install("optparse",ask=FALSE,force=TRUE,update=FALSE)
-BiocManager::install(c("readr", "magrittr", "tidyr", "jsonlite","data.table"),ask=FALSE,force=TRUE,update=FALSE)
-BiocManager::install("dada2",ask=FALSE,force=TRUE,update=FALSE)
+# Install remotes to handle older versions
+install.packages("remotes")
+
+# Install the correct versions of MASS and Matrix
+remotes::install_version("MASS", version = "7.3-55", repos = "https://cloud.r-project.org")
+remotes::install_version("Matrix", version = "1.5-1", repos = "https://cloud.r-project.org")
+
+# Install dada2 without updating other packages
+BiocManager::install("dada2", ask=FALSE, force=TRUE, update=FALSE)
