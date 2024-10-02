8c1070e2642c8fe79c7f7df1c4bfb2025ce005c0
Khaled Altabtbaei <36083988+khalidtab@users.noreply.github.com>
2024-10-02 08:21:33 -0600

    DADA2 installation pinning, with various libraries installed

--- workflow/envs/install_packages.R
+++ workflow/envs/install_packages.R
@@ -6,5 +6,5 @@
 #devtools::install_github("benjjneb/dada2")
 
 BiocManager::install("optparse",ask=FALSE,force=TRUE,update=FALSE)
-BiocManager::install(c("readr", "magrittr", "tidyr", "jsonlite","data.table","dplyr"),ask=FALSE,force=TRUE,update=FALSE)
-BiocManager::install(c("mgcv", "DelayedArray", "SummarizedExperiment", "GenomicAlignments", "ShortRead", "ggplot2", "dada2"),ask=FALSE,force=TRUE,update=FALSE)
+BiocManager::install(c("readr", "magrittr", "tidyr", "jsonlite","data.table"),ask=FALSE,force=TRUE,update=FALSE)
+BiocManager::install("dada2",ask=FALSE,force=TRUE,update=FALSE)
